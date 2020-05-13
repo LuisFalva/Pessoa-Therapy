027c3158c2842a9898597eaf0c0ecb687714679a
luiscarlos.vargas.alvarez <luiscarlos.vargas.alvarez@bbva.com>
2020-05-13 15:59:23 -0500

    [ADD] commit new files project

---
+++ src/Module.java
@@ -0,0 +1,7 @@
+public class Module {
+
+    int ddof = 1;
+    double[] doseArray = { 0.02, 0.024, 0.03, 0.037, 0.043, 0.04, 0.025, 0.027, 0.019, 0.019, 0.018, 0.017 };
+    int[][] matrix_arr = new int[0][0];
+
+}
